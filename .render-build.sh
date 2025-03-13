b09c9b4314d404a2a39489b58e61a52fea2da086
R-G2110 <argeldelacruz86@gmail.com>
2025-03-13 23:53:51 +0100

    Reso eseguibile il file render-build.sh

--- .render-build.sh
+++ .render-build.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-set -e  # Ferma l'esecuzione se un comando fallisce
 
-# Installa le dipendenze PHP
-echo "Running Composer install..."
+# Install dependencies
 composer install --no-dev --optimize-autoloader
 
-# Esegui le migrazioni (se necessarie)
-echo "Running Artisan Migrations..."
+# Run migrations
 php artisan migrate --force
-
-
